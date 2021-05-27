31eea2526e5bfae1251e4edadf389993f39c34c2
bev-ly <83798546+bev-ly@users.noreply.github.com>
2021-05-27 14:57:17 +0800

    Add files via upload

---
+++ LOOPAss1.java
@@ -0,0 +1,20 @@
+
+package loopass1;
+import java.util.Scanner;
+
+public class LOOPAss1 {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int b = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter Number ");
+        b = sc.nextInt();
+        
+        for (int i = 0; i <= b ; i++){
+            System.out.println(i + "");
+        }
+    }
+    
+}
